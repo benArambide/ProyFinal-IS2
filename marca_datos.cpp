3cc3479d4abd45bd452bf79f41d19504dde2caf3
Josue <josue@josue-AN-7121.(none)>
2012-10-01 21:28:54 -0500

    Agragando las funciones a todos los botones

---
+++ marca_datos.cpp
@@ -0,0 +1,5 @@
+#include "marca_datos.h"
+
+Marca_Datos::Marca_Datos()
+{
+}
---
+++ marca_datos.h
@@ -0,0 +1,12 @@
+#ifndef MARCA_DATOS_H
+#define MARCA_DATOS_H
+#include <QSqlQuery>
+
+class Marca_Datos
+{
+public:
+    Marca_Datos();
+
+};
+
+#endif // MARCA_DATOS_H
